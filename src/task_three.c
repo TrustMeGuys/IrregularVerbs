d01dfc8331384104d282020925d31f172c9f5016
Vlad Zarechnev <vlad.zarechnev.01@mail.ru>
2020-05-28 16:42:34 +0700

    Added the header file IrregularVerbs.h initialization

--- src/task_three.c
+++ src/task_three.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "IrregularVerbs.h"
 #define B 90
 
 int task_three()
 {
     printf("Выберите номер предложения, которое хотите перевести:\n");
-    int x = choose_your_number();
+    int num_sent = choose_your_number();
     FILE* three_task;
     three_task = fopen("../text_file/three_task.txt", "r");
     if (three_task == NULL) {
@@ -12,19 +12,19 @@ int task_three()
         return -1;
     }
     char ch[B] = {0}, ch1[B] = {0};
-    for (int i = 1; i < x + 1; i++) {
-        if (i == x) {
+    for (int i = 1; i < num_sent + 1; i++) {
+        if (i == num_sent) {
             fgets(ch1, B, three_task);
             printf("%s\n", ch1);
         }
         fgets(ch, B, three_task);
     }
-    variant_answer(x);
+    variant_answer(num_sent);
     fclose(three_task);
     return 0;
 }
 
-int variant_answer(int x)
+int variant_answer(int num_sent)
 {
     FILE* answer_third;
     answer_third
@@ -36,27 +36,27 @@ int variant_answer(int x)
         return 1;
     }
     char ch[B] = {0}, ch1[B] = {0};
-    for (int i = 1; i < x + 1; i++) {
-        if (i == x) {
+    for (int i = 1; i < num_sent + 1; i++) {
+        if (i == num_sent) {
             fgets(ch1, B, answer_third);
             printf("%s\n", ch1);
         }
         fgets(ch, B, answer_third);
     }
     fclose(answer_third);
-    check_answer(x);
+    check_answer(num_sent);
     return 0;
 }
 
-int check_answer(int x)
+int check_answer(int num_sent)
 {
     int f;
     int* answer_user;
-    if (x % 2 == 0) {
+    if (num_sent % 2 == 0) {
         f = 5;
-    } else if (x % 3 == 0 && x % 2 != 0) {
+    } else if (num_sent % 3 == 0 && num_sent % 2 != 0) {
         f = 8;
-    } else if (x % 2 != 0 && x % 3 != 0) {
+    } else if (num_sent % 2 != 0 && num_sent % 3 != 0) {
         f = 10;
     }
     answer_user = (int*)malloc(f * sizeof(int));
@@ -64,8 +64,8 @@ int check_answer(int x)
     for (int i = 0; i < f; i++) {
         scanf("%d", &answer_user[i]);
     }
-    if (x % 2 == 0) {
-        int z = check_user_answer(x, answer_user);
+    if (num_sent % 2 == 0) {
+        int z = check_user_answer(num_sent, answer_user);
         if (z == 5) {
             printf("Мои поздравления!\n");
             continue_task(3);
@@ -73,8 +73,8 @@ int check_answer(int x)
             printf("Number of matched numbers:%d\n", z);
             continue_task(3);
         }
-    } else if (x % 3 == 0 && x % 2 != 0) {
-        int z = check_user_answer(x, answer_user);
+    } else if (num_sent % 3 == 0 && num_sent % 2 != 0) {
+        int z = check_user_answer(num_sent, answer_user);
         if (z == 8) {
             printf("Мои поздравления!\n");
             continue_task(3);
@@ -82,8 +82,8 @@ int check_answer(int x)
             printf("Number of matched numbers:%d\n", z);
             continue_task(3);
         }
-    } else if (x % 2 != 0 && x % 3 != 0) {
-        int z = check_user_answer(x, answer_user);
+    } else if (num_sent % 2 != 0 && num_sent % 3 != 0) {
+        int z = check_user_answer(num_sent, answer_user);
         if (z == 10) {
             printf("Мои поздравления!\n");
             continue_task(3);
@@ -98,7 +98,7 @@ int check_answer(int x)
 int check_user_answer(int x, int* answer_user)
 {
     int z = 0;
-    if (x % 2 == 0) {
+    if (num_sent % 2 == 0) {
         int variant1_1[5] = {2, 3, 1, 4, 5}, variant1_2[5] = {1, 5, 4, 3, 2},
             variant1_3[5] = {3, 4, 5, 2, 1};
         for (int i = 0; i < 5; i++) {
@@ -107,7 +107,7 @@ int check_user_answer(int x, int* answer_user)
                 || variant1_3[i] == answer_user[i])
                 z++;
         }
-    } else if (x % 3 == 0 && x % 2 != 0) {
+    } else if (num_sent % 3 == 0 && x % 2 != 0) {
         int variant2_1[8] = {6, 7, 1, 2, 3, 5, 4, 8},
             variant2_2[8] = {8, 1, 5, 3, 4, 2, 7, 6},
             variant2_3[8] = {3, 8, 6, 1, 2, 4, 5, 7};
@@ -117,7 +117,7 @@ int check_user_answer(int x, int* answer_user)
                 || variant2_3[i] == answer_user[i])
                 z++;
         }
-    } else if (x % 2 != 0 && x % 3 != 0) {
+    } else if (num_sent % 2 != 0 && num_sent % 3 != 0) {
         int variant3_1[10] = {10, 2, 3, 5, 4, 9, 8, 7, 6, 1},
             variant3_2[10] = {1, 9, 8, 7, 10, 6, 4, 5, 3, 2},
             variant3_3[10] = {2, 10, 9, 6, 5, 8, 7, 3, 1, 4};
@@ -131,13 +131,13 @@ int check_user_answer(int x, int* answer_user)
     return z;
 }
 
-int return_task_three(int x)
+int return_task_three(int num_sent)
 {
     if (x == 0)
         exit(EXIT_SUCCESS);
-    else if (x == 1)
+    else if (num_sent == 1)
         main();
-    else if (x == 2)
+    else if (num_sent == 2)
         task_three();
     else {
         printf("Вы ввели что-то другое...");
