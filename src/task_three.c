eaf9b1e93a0c021ac8ffcd2b9387a0f9fe8a01ec
Vlad Zarechnev <vlad.zarechnev.01@mail.ru>
2020-05-30 19:24:54 +0700

    Add the check_user_answer function, for the file to work task_three.c

--- src/task_three.c
+++ src/task_three.c
@@ -5,6 +5,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+int check_user_answer(int num_sent, int* answer_user)
+{
+    int z = 0;
+    if (num_sent % 2 == 0) {
+        int variant1_1[5] = {2, 3, 1, 4, 5}, variant1_2[5] = {1, 5, 4, 3, 2},
+            variant1_3[5] = {3, 4, 5, 2, 1};
+        for (int i = 0; i < 5; i++) {
+            if (variant1_1[i] == answer_user[i]
+                || variant1_2[i] == answer_user[i]
+                || variant1_3[i] == answer_user[i])
+                z++;
+        }
+    } else if (num_sent % 3 == 0 && num_sent % 2 != 0) {
+        int variant2_1[8] = {6, 7, 1, 2, 3, 5, 4, 8},
+            variant2_2[8] = {8, 1, 5, 3, 4, 2, 7, 6},
+            variant2_3[8] = {3, 8, 6, 1, 2, 4, 5, 7};
+        for (int i = 0; i < 8; i++) {
+            if (variant2_1[i] == answer_user[i]
+                || variant2_2[i] == answer_user[i]
+                || variant2_3[i] == answer_user[i])
+                z++;
+        }
+    } else if (num_sent % 2 != 0 && num_sent % 3 != 0) {
+        int variant3_1[10] = {10, 2, 3, 5, 4, 9, 8, 7, 6, 1},
+            variant3_2[10] = {1, 9, 8, 7, 10, 6, 4, 5, 3, 2},
+            variant3_3[10] = {2, 10, 9, 6, 5, 8, 7, 3, 1, 4};
+        for (int i = 0; i < 10; i++) {
+            if (variant3_1[i] == answer_user[i]
+                || variant3_2[i] == answer_user[i]
+                || variant3_3[i] == answer_user[i])
+                z++;
+        }
+    }
+    return z;
+}
+
 int check_answer(int num_sent)
 {
     int f;
