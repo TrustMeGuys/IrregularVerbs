feb99328d95237dd64b2005392731bc1ef063dd3
Vlad Zarechnev <vlad.zarechnev.01@mail.ru>
2020-06-19 23:03:15 +0700

    Created new functions for reading files, changed old ones

--- src/task_three.h
+++ src/task_three.h
@@ -5,6 +5,8 @@
 int main();
 int check_user_answer(int num_sent, int* answer_user);
 int check_answer(int num_sent);
+int work_in_variant_answer_third_file(int num_sent);
+int work_in_three_task_file(int num_sent);
 int variant_answer(int num_sent);
 int task_three();
 int return_task_three(int num_sent);
